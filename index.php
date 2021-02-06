c058cca932fe779d05482dcf70b585bb39252e03
jaybaybremner <j.bremner@ymail.com>
2021-02-06 11:39:23 +0000

    fist commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+  include('lib/config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'sections/head.php'
+  ?>
+<body>
+
+
+  <?php include 'sections/header.php'
+  ?>
+
+  <?php include 'sections/footer.php'
+  ?>
+</body>
+</html>
