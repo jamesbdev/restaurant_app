dba60c827ac0ef3267104c62e66d498ffee434d2
jaybaybremner <j.bremner@ymail.com>
2021-02-20 11:47:34 +0000

    add about us page

---
+++ about-us.php
@@ -0,0 +1,16 @@
+<?php 
+  include('lib/config.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include 'sections/head.php'
+  ?>
+</head>
+<body> 
+   <?php include 'sections/header.php'?>
+   <?php include 'sections/about.php'?>
+</body>
+</html>
