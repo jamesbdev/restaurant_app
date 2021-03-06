007cb41628e358caac1d733c85ea8be9089d81f8
jaybaybremner <j.bremner@ymail.com>
2021-03-06 12:34:25 +0000

    adds terms content

---
+++ sections/terms.php
@@ -0,0 +1,2 @@
+<h1>Terms and Conditions</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sequi doloribus impedit. Alias sapiente natus iusto aperiam aut nisi et tenetur quod illo placeat hic magnam esse, sequi commodi amet!</p>
